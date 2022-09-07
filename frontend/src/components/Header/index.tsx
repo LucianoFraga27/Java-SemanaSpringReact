197c94a4964a75d9dd87da4447bcb4763afdd98e
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-09-07 17:28:01 -0300

    SecurityConfig

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import './styles.css'
-import logo from '../../assets/img/logo.svg'
+import './styles.css';
+import logo from '../../assets/img/logo.svg';
+
 function Header() {
 
     return (
@@ -18,4 +19,4 @@ function Header() {
 
 }
 
-export default Header
+export default Header;
